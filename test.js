64edf75b884500ba07c85ff3ea00c82044d90df9
Chad Vink <ChadVink@Chads-MacBook-Pro.local>
2015-09-27 15:11:06 -0700

    Two paths drawn

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
-console.log("Nothing to see here.");
+console.log("Bezier Test");
+
+var lineData = [ { "x": 1,   "y": 5},  { "x": 20,  "y": 20},
+                 { "x": 40,  "y": 10}, { "x": 60,  "y": 40},
+                 { "x": 80,  "y": 5},  { "x": 100, "y": 60}];
+
+var lineFunction = d3.svg.line()
+                        .x( function(d){ return d.x;} )
+                        .y( function(d){ return d.y;} )
+                        .interpolate("linear");
+
+var svgContainer = d3.select("body").append("svg")
+                                    .attr("height", 200)
+                                    .attr("width", 200);
+
+var lineGraph = svgContainer.append("path")
+                            .attr("d", lineFunction(lineData))
+                            .attr("stroke", "blue")
+                            .attr("stroke-width", 4)
+                            .attr("fill", "none");
